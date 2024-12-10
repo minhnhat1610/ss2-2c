e66a4f3a7c394eab8222ef407591636dec605329
minhnhat1610 <161536740+minhnhat1610@users.noreply.github.com>
2024-12-10 18:52:01 +0700

    Add files via upload

---
+++ ss1-2c.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    int a;
+
+    printf("Nhap so can kiem tra ");
+    scanf("%d",&a);
+
+    if (a > 5)
+    {
+        printf("dat");
+    } else 
+    {
+        printf("khong dat");
+    }
+    
+    return 0;
+}
